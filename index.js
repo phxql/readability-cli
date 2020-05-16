1ac98e3d3cdc0b6372ea13607564dc005a4bcbf7
NightMachinary <rudiwillalwaysloveyou@gmail.com>
2020-05-16 15:41:27 +0430

    Added proper stdin support. Added sanitization. Last commit to accept URLs and files as input.

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 #!/usr/bin/env node
 
-const program = require('commander');
-const jsdom = require('jsdom');
-const pkg = require('./package.json');
-const Readability = require('readability');
+const debug = Boolean(process.env.DEBUGME)
 
+const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
 
 const jsdomConsole = new jsdom.VirtualConsole();
 
 // Suppress these errors for now
-jsdomConsole.on('jsdomError', () => {});
+jsdomConsole.on('jsdomError', () => { });
 
 const options = {
   features: {
@@ -19,6 +17,14 @@ const options = {
   },
   virtualConsole: jsdomConsole,
 };
+
+const createDOMPurify = require('dompurify');
+const window = new JSDOM('').window;
+const DOMPurify = createDOMPurify(window);
+const sanitizeHtml = require('sanitize-html');
+const program = require('commander');
+const pkg = require('./package.json');
+const Readability = require('readability');
 
 const readability = (dom) => {
   // Happens on missing file
@@ -29,7 +35,13 @@ const readability = (dom) => {
     console.error('Error: Readability returned nothing. This usually happens on empty input');
     return;
   }
-  console.log('<h1>', article.title, '</h1>', article.content);
+  if (debug) {
+    console.error(JSON.stringify(article));
+  }
+  if (article.title) {
+    console.log('<h1>', sanitizeHtml(article.title), '</h1>')
+  }
+  console.log(sanitizeHtml(article.content));
 };
 
 const isURL = (str) => {
@@ -42,8 +54,8 @@ const handleError = err => console.error(err.toString());
 const run = (sources) => {
   const promises = sources.map(source =>
     (isURL(source) ?
-      JSDOM.fromURL(source, options) :
-      JSDOM.fromFile(source, options)
+      DOMPurify.fromURL(source, options) :
+      DOMPurify.fromFile(source, options)
     ));
   Promise.all(promises).then((doms) => {
     doms.forEach(readability);
@@ -58,10 +70,12 @@ program
   .action(run)
   .parse(process.argv);
 
-if (program.args.length === 0) {
-  if (!process.stdin.isTTY) {
-    JSDOM.fromFile('/dev/stdin', options).then(readability).catch(handleError);
-  } else {
-    program.outputHelp();
+(async () => {
+  if (program.args.length === 0) {
+    const getStdin = require('get-stdin');
+    var doc = await getStdin();
+
+    const clean = DOMPurify.sanitize(doc);
+    readability(new JSDOM(clean, options));
   }
-}
+})();
